07033d8fc834fba62c5c1598cc37981b53bf32f6
Debabrata Ghosh <121720800+SUPERREALCODER@users.noreply.github.com>
2024-05-24 09:08:43 +0000

    resume kind

--- 4.3+EJS+Partials/4.3 EJS Partials/index.js
+++ 4.3+EJS+Partials/4.3 EJS Partials/index.js
@@ -2,6 +2,17 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+  res.render("index.ejs");
+});
+app.get("/about",(req,res)=>{
+  res.render("about.ejs");
+});
+app.get("/contact",(req,res)=>{
+  res.render("contact.ejs");
+});
 
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
