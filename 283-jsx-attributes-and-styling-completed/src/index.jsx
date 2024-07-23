fc4bc3078412b5dc2e47f65893ecefa55eff2dcd
Debabrata Ghosh <121720800+SUPERREALCODER@users.noreply.github.com>
2024-07-23 07:33:28 +0530

    added styling jsx
    Signed-off-by: Debabrata Ghosh <121720800+SUPERREALCODER@users.noreply.github.com>

---
+++ 283-jsx-attributes-and-styling-completed/src/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <img alt="random" src={img + "?grayscale"} />
+
+    <img
+      className="food-img"
+      alt="bacon"
+      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*"
+    />
+    <img
+      className="food-img"
+      alt="jamon"
+      src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"
+    />
+    <img
+      className="food-img"
+      alt="noodles"
+      src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"
+    />
+  </div>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
