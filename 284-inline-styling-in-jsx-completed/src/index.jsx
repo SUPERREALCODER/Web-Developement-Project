5611627cfc9d35cf8085ce6e6035836aa8f160c3
Debabrata Ghosh <121720800+SUPERREALCODER@users.noreply.github.com>
2024-07-24 07:39:50 +0530

    Add files via upload
    Signed-off-by: Debabrata Ghosh <121720800+SUPERREALCODER@users.noreply.github.com>

---
+++ 284-inline-styling-in-jsx-completed/src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+};
+
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
