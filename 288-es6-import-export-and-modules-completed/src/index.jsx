4c91001ae49dd8196960b348ba58d009c226d47c
Debabrata Ghosh <121720800+SUPERREALCODER@users.noreply.github.com>
2024-07-30 08:03:49 +0530

    es6 import export
    Signed-off-by: Debabrata Ghosh <121720800+SUPERREALCODER@users.noreply.github.com>

---
+++ 288-es6-import-export-and-modules-completed/src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import pi, { doublePi, triplePi } from "./math.js";
+
+ReactDOM.render(
+  <ul>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
