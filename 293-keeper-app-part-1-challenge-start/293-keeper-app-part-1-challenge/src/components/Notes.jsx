3ff0b1cdb35c306a216dcb8dd1a1656654abd22a
SUPERREALCODER <debabratag542@gmail.com>
2024-08-01 07:50:16 +0530

    keeper app

---
+++ 293-keeper-app-part-1-challenge-start/293-keeper-app-part-1-challenge/src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes(){
+    return(
+        <div className="note">
+            <h1>This is the Note title</h1>
+            <p>This is the note content</p>
+        </div>
+
+    );
+}
+export default Notes;
